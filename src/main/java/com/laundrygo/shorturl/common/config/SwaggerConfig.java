d0d121ec93ffe487283ceaa9b70bf8ba5bc067ca
seungduru <dlatmddn468@gmail.com>
2024-12-01 00:56:12 +0900

    feat: JpaConfig, SwaggerConfig 생성

---
+++ src/main/java/com/laundrygo/shorturl/common/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.laundrygo.shorturl.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Short URL API")
+                        .version("1.0.0")
+                        .description("This is the API documentation for the Short URL project."));
+    }
+}
