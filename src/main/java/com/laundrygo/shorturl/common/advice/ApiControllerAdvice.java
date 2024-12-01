a9df7d80b7d3bad4ba47a10a769212808d704f25
seungduru <dlatmddn468@gmail.com>
2024-12-01 21:50:45 +0900

    refactor: ControllerAdvice 에 Validation 예외 추가

--- src/main/java/com/laundrygo/shorturl/common/advice/ApiControllerAdvice.java
+++ src/main/java/com/laundrygo/shorturl/common/advice/ApiControllerAdvice.java
@@ -4,11 +4,30 @@ import com.laundrygo.shorturl.common.exception.ErrorCode;
 import com.laundrygo.shorturl.common.exception.LgShortUrlException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static com.laundrygo.shorturl.common.constants.CommonConstants.VALIDATION_FORMAT;
+
 @RestControllerAdvice
 public class ApiControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Object>> handleValidationException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> String.format(VALIDATION_FORMAT, error.getField(), error.getDefaultMessage()))
+                .findFirst()
+                .orElse("Invalid request");
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(HttpStatus.BAD_REQUEST, errorMessage));
+    }
+
+
     @ExceptionHandler(LgShortUrlException.class)
     public ResponseEntity<ApiResponse<Object>> handleShortUrlException(LgShortUrlException ex) {
         ErrorCode errorCode = ex.getErrorCode();
--- src/main/java/com/laundrygo/shorturl/common/constants/CommonConstants.java
+++ src/main/java/com/laundrygo/shorturl/common/constants/CommonConstants.java
@@ -6,6 +6,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public abstract class CommonConstants {
 
+    // String Format
+    public static final String VALIDATION_FORMAT = "%s: %s";
+
+    // Numeric
     public static final int EIGHT = 8;
 
 }
